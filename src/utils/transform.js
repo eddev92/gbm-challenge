948d10c2ff9bc376a8b169c350aa96a11ca77150
Edward David Llanca Haro <ellanca@Macmini-de-ELLANCA.local>
2018-11-11 14:16:28 -0500

    add validation rol nav component

--- src/utils/transform.js
+++ src/utils/transform.js
@@ -1,6 +1,25 @@
 import CustomDate from "./custom-date";
 
 class TransformData {
+    static getRole(role) {
+        let rol = '';
+        switch (role) {
+            case 'EXECUTIVE': 
+                rol = 'Ejecutivo'
+                break;
+            case 'ASSISTANT': 
+                rol = 'Asistente'
+                break;
+            case 'ADMIN': 
+                rol = 'Administrador'
+                break;
+            default:
+                rol = ''
+                break;
+        }
+        return rol;
+
+    }
     static getCols(obj) {
         const dateFormated = obj.Fecha.length && CustomDate.getSimpleDateFormatMin(obj.Fecha);
         return [
